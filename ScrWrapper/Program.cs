eaab6b2a5f9b9347577f55b80bda822eb9a7b917
AmateurLabs <amateurlabs@gmail.com>
2014-09-22 00:58:42 -0500

    Added ScrWrapper

---
+++ ScrWrapper/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AmateurLabs.CandyWrapper.Scr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = Process.GetCurrentProcess().MainModule.FileName;
+            string directory = Path.GetDirectoryName(path);
+            string filename = Path.GetFileNameWithoutExtension(path);
+
+            Environment.CurrentDirectory = directory;
+
+            string configPath = "config.txt";
+            if (!File.Exists(configPath) && Directory.Exists("Data/") && File.Exists("Data/config.txt"))
+                configPath = "Data/config.txt";
+
+            string executable = filename + ".exe";
+            bool handleDefault = true;
+            bool handlePreview = false;
+            bool handleSettings = false;
+
+            if (File.Exists(configPath)) {
+                string[] lines = File.ReadAllLines(configPath);
+                foreach (string line in lines)
+                {
+                    string key = line.Substring(0, line.IndexOf('='));
+                    string value = line.Substring(line.IndexOf('='));
+                    if (key == "executable") executable = value;
+                    else if (key == "handleDefault") handleDefault = value == "true";
+                    else if (key == "handlePreview") handlePreview = value == "true";
+                    else if (key == "handleSettings") handleSettings = value == "true";
+                }
+            }
+
+            bool run = false;
+
+            foreach (string arg in args)
+            {
+                if (arg.StartsWith("/s") && handleDefault) run = true;
+                if (arg.StartsWith("/p") && handlePreview) run = true;
+                if (arg.StartsWith("/l") && handlePreview) run = true;
+                if (arg.StartsWith("/c") && handleSettings) run = true;
+            }
+
+            if (!run) return;
+            ProcessStartInfo info = new ProcessStartInfo(executable, string.Join(" ", args));
+            info.WorkingDirectory = directory;
+            Process process = Process.Start(info);
+            process.WaitForExit();
+        }
+    }
+}
